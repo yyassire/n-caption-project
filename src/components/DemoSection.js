b8923f61f956867ba3224e70268b996dc973fa50
yassire yaguibou <yyassire02@gmail.com>
2023-11-07 11:28:24 +0300

    first commit

---
+++ src/components/DemoSection.js
@@ -0,0 +1,17 @@
+import SparklesIcon from "@/components/SparklesIcon";
+
+export default function DemoSection() {
+  return (
+    <section className="flex justify-around mt-8 sm:mt-12 items-center">
+      <div className="hidden sm:block bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
+        <video preload="true" src="/vids/without-captions.mp4"  muted autoPlay loop></video>
+      </div>
+      <div className="hidden sm:block">
+        <SparklesIcon />
+      </div>
+      <div className="bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
+        <video preload="true" src="/vids/with-captions.mp4"  muted autoPlay loop></video>
+      </div>
+    </section>
+  );
+}
